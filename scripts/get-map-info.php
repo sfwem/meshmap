8abe7a89efe3eec06102ccd601c2f67d98e8a26d
Eric Satterlee - KG6WXC <kg6wxc@gmail.com>
2018-04-07 13:51:02 -0700

    removed what git added in get-map-info.php (still learning this git stuff :) )

--- scripts/get-map-info.php
+++ scripts/get-map-info.php
@@ -1,10 +1,6 @@
 #!/usr/bin/env php
 <?php $mtimeStart = microtime(true);
-<<<<<<< HEAD
-//
-=======
 //testing
->>>>>>> refs/remotes/origin/master
 /*************************************************************************************
 * get-map-info script v2 by kg6wxc\eric satterlee kg6wxc@gmail.com
 * This script is the heart of kg6wxcs' mesh map system.
